04a7e44cb1fa3fc4370c2e71fbd3436d08809bd7
iXuacu <xuacu2k@hotmail.com>
2021-03-16 17:09:21 +0100

    Initial commit

---
+++ A quien le toca!/A quien le toca!/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace A_quien_le_toca_
+{
+    public partial class MainPage : ContentPage
+    {
+        float saldoXuacu = 1;
+        float saldoToni = 0;
+        float incrementoSaldo = 0;
+        string aQuienLeToca;
+        public MainPage()
+        {
+            InitializeComponent();
+            lblSaldoToni.Text = "Saldo Toni: " + saldoToni.ToString();
+            lblSaldoXuacu.Text = "Saldo Xuacu: " + saldoXuacu.ToString();
+            CalculaPago();
+        }
+
+        private void CalculaPago()
+        {
+            if (saldoXuacu < saldoToni)
+            {
+                aQuienLeToca = "Paga Xuacu";
+                saldoXuacu += incrementoSaldo;
+                lblSaldoXuacu.Text = "Saldo Xuacu: " + saldoXuacu.ToString();            
+            }
+            else
+            {
+                aQuienLeToca = "Paga Toni";
+                saldoToni += incrementoSaldo;
+                lblSaldoToni.Text = "Saldo Toni: " + saldoToni.ToString();
+            }
+            entCantidad.Text = "";
+            lblPaga.Text = aQuienLeToca;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            incrementoSaldo = float.Parse(entCantidad.Text);
+            CalculaPago();
+            
+        }
+    }
+}
